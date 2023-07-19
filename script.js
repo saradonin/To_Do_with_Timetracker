8c9e36f8452c79d8aa0e586102aba477782710f3
saradonin <m.jemieljanczuk@gmail.com>
2023-07-19 22:25:26 +0200

    apiListTasks and apiCreateTask created, renderTask skeleton created

--- script.js
+++ script.js
@@ -2,8 +2,82 @@ const apikey = '93a5a0f8-07f6-41f7-ab39-c61a1207f499';
 const apihost = 'https://todo-api.coderslab.pl';
 
 
+function apiListTasks() {
+    // GET method
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            headers: {Authorization: apikey}
+        }
+    ).then(
+        function (resp) {
+            if (!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    )
+}
+
+function renderTask(taskId, title, description, status) {
+  const section = document.createElement("section");
+  section.className = 'card mt-5 shadow-sm';
+
+  // tylko na chwilę ustawimy taką treść:
+  section.innerText = 'Tytuł: ' + title + ', opis: ' + description + ', status: ' + status;
+
+  document.querySelector('main').appendChild(section);
+}
 
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+function apiCreateTask(title, description) {
+    // uses POST method
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            method: "POST",
+            headers: {
+                'Authorization': apikey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title: title, description: description, status: 'open'})
+        }
+    ).then(
+        function (resp) {
+            if (!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    )
+}
+
+//
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    apiListTasks().then(
+        function (response) {
+
+            // "response" zawiera obiekt z kluczami "error" i "data" (zob. wyżej)
+            // "data" to tablica obiektów-zadań
+            // uruchamiamy funkcję renderTask dla każdego zadania jakie dał nam backend
+            response.data.forEach(
+                function (task) {
+                    renderTask(task.id, task.title, task.description, task.status);
+                }
+            );
+        }
+    );
+
+
+    apiCreateTask('Przykładowy tytuł', 'Przykładowy opis').then(
+        function (response) {
+            console.log('Odpowiedź z serwera to:', response);
+        }
+    );
+
 
 });
